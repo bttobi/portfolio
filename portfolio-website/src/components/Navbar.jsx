a5200e6557ed96c9cdb1cfa7a6c2fc837d4b8d14
bttobi <bttobi123@gmail.com>
2023-03-25 22:23:18 +0100

    Update Navbar.jsx

--- portfolio-website/src/components/Navbar.jsx
+++ portfolio-website/src/components/Navbar.jsx
@@ -4,9 +4,11 @@ import { NavLink } from 'react-router-dom';
 const Navbar = () => {
   return (
     <nav className="navbar backdrop-blur-sm bg-white/5 flex flex-row align-center justify-start fixed m-0 p-0 text-white w-full h-20 z-20 text-4xl">
-      <div className="home-wrapper h-full flex grow whitespace-nowrap">
-        <img src="./logo.png" alt="bartosz tobinski logo picture" width="70px"/>
-        <NavLink to="/" className="name-surname home-page font-bold ml-6">BARTOSZ TOBIŃSKI</NavLink>
+      <div className="home-wrapper h-full flex justify-center grow whitespace-nowrap">
+        <NavLink to="/" className="name-surname home-page font-bold ml-6 flex align-center justify-center items-center">
+          <img className="logo ml-4" src="./logo.png" alt="bartosz tobinski logo picture" width="70px"/>
+          <p className="name-and-surname ml-4">BARTOSZ TOBIŃSKI</p>
+        </NavLink>
       </div>
       <div className="navlinks-wrapper h-full flex flex-row w-full justify-around">
         <NavLink to="/projects" className="projects-link">PROJECTS</NavLink>
