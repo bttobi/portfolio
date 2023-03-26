1abf2548a76432114d507c067ef94c216f4ee110
bttobi <bttobi123@gmail.com>
2023-03-26 22:58:29 +0200

    removed routing

--- portfolio-website/src/App.jsx
+++ portfolio-website/src/App.jsx
@@ -1,23 +1,17 @@
 import './index.css';
-import './components/Navbar'
-import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Navbar from './components/Navbar';
-import Home from './components/Home';
-import Projects from './components/Projects';
-import Contact from './components/Contact';
+import Home from './components/pages/Home';
+import Projects from './components/pages/Projects';
+import Contact from './components/pages/Contact';
 
 function App() {
   return (
-    <BrowserRouter>
       <div className="App">
         <Navbar/>
-        <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='projects' element={<Projects/>}/>
-          <Route path='contact' element={<Contact/>}/>
-        </Routes>
+        <Home/>
+        <Projects/>
+        <Contact/>
       </div>
-    </BrowserRouter>
   )
 }
 
