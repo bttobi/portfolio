d18ccdfb782ee89e37aa8784f29c9ef0d89b815c
bttobi <bttobi123@gmail.com>
2023-03-31 17:07:16 +0200

    made cat disp scene with animated model

--- portfolio-website/src/components/scenes/CatDispScene.jsx
+++ portfolio-website/src/components/scenes/CatDispScene.jsx
@@ -1,19 +1,19 @@
 import React, { Suspense, useRef } from 'react';
 import { OrbitControls } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
-import HelloText from './models/HelloText';
+import CatDispenser from './models/CatDispenser';
 
-const HelloScene = () => {
+const CatDispScene = () => {
   return (
   <Suspense fallback={null}>
-    <Canvas className="flex" style={{width: "50%", height: "100vh"}}>
+    <Canvas className="flex" style={{width: "100%", height: "100%"}}>
       <ambientLight intensity={0.5}/>
       <pointLight intensity={2} position={[10,0,10]} />
       <OrbitControls enableZoom={false}/>
-      <HelloText />
+      <CatDispenser />
     </Canvas>
   </Suspense>
   )
 }
 
-export default HelloScene
+export default CatDispScene;
