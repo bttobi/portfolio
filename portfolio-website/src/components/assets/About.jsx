d5ca4762893ee3951ab1ddc1424de748dbdeab3e
bttobi <bttobi123@gmail.com>
2023-04-01 23:53:59 +0200

    mobile responsive

--- portfolio-website/src/components/assets/About.jsx
+++ portfolio-website/src/components/assets/About.jsx
@@ -2,14 +2,15 @@ import React from 'react';
 
 const About = () => {
   return (
-    <div className="mockup-code mt-12 text-white text-2xl flex flex-col w-1/2 mr-4 ml-16 shadow-lg shadow-black" style={{backgroundColor: "#222831"}}>
+    <div className="mockup-code order-2 lg:order-1 text-white text-2xl flex flex-col mx-12 w-2/3 lg:w-1/2 shadow-lg shadow-black" style={{backgroundColor: "#222831"}}>
       <div className="avatar w-full align-center justify-center">
         <div className="w-44 h-44 mt-4 rounded-full ring ring-blue-dark backdrop-blur ring-offset-base-100 ring-offset-2">
           <img className="blur-0" src="./photo.jpg" alt="profile picture" />
         </div>
       </div>
       <article className="bio-description mt-8 text-left">
-        <p className="mx-8">Hi! I’m a front end web developer and an ICT student at Poznań University of Technology based in Poland.</p>
+        <p className="mx-8">Hi! I’m a front-end web developer and an ICT student at Poznań University of Technology based in Poland.
+        </p>
         <br />
         <p className="mx-8">Currently working at GSK.</p>
       </article>
