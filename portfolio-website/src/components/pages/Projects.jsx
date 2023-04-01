9a5c6e0cb22f70766010a2abadbecc5b75094165
bttobi <bttobi123@gmail.com>
2023-04-02 01:20:53 +0200

    mobile responsive tweaks

--- portfolio-website/src/components/pages/Projects.jsx
+++ portfolio-website/src/components/pages/Projects.jsx
@@ -7,9 +7,9 @@ const Projects = () => {
 
   return (
     <div id="projects" style={{height: "100vh", width: "100%"}} className="article">
-      <p className="font-bold text-2xl pt-32 text-center">Selected projects:</p>
+      <p className="font-bold text-2xl pt-24 lg:pt-32 text-center">Selected projects:</p>
       <p className="text-xl text-center">Visit my Github for more</p>
-      <div className="projects-wrapper w-full h-1/3 flex lg:flex-row flex-col items-center justify-evenly mt-48">
+      <div className="projects-wrapper w-full h-2/3 flex lg:flex-row flex-col items-center justify-center mt-8 lg:mt-16">
         {projects.map(el => <ProjectCard projectInfo={el} key={el.id}/>)}
       </div>
     </div>
