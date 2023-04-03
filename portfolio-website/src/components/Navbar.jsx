d3ced98b71c39aa22377ffc3b5e9fc69a57c6dc2
bttobi <bttobi123@gmail.com>
2023-04-03 22:40:21 +0200

    added working hamburger - yet to be styled

--- portfolio-website/src/components/Navbar.jsx
+++ portfolio-website/src/components/Navbar.jsx
@@ -1,13 +1,16 @@
-import React, { useState, useEffect } from 'react';
-import { GiHamburgerMenu } from "react-icons/gi";
+import React, { useState, useRef } from 'react';
 
 const Navbar = () => {
-
+  const hamburgerRef = useRef();
+  const [hamburgerIsActive, setHamburgerIsActive] = useState(false);
+  const changeVisibility = () => {
+    setHamburgerIsActive(hamburgerRef?.current?.checked)
+  }
   return (
     <nav className="navbar backdrop-blur-sm bg-white/10 flex flex-row align-center justify-start fixed m-0 p-0 text-white w-full h-10 z-20 text-3xl">
       <div className="home-wrapper h-full flex justify-center grow whitespace-nowrap">
         <a href="#home" className="name-surname home-page font-bold ml-6 flex align-center justify-center items-center">
-          <img className="logo ml-4" src="./logo.png" alt="bartosz tobinski logo picture" width="60px"/>
+          <img className="logo lg:ml-4" src="./logo.png" alt="bartosz tobinski logo picture" width="60px"/>
           <p className="nav-button name-and-surname ml-4 p-1 hidden lg:block rounded-lg">BARTOSZ TOBIŃSKI</p>
         </a>
       </div>
@@ -16,9 +19,14 @@ const Navbar = () => {
         <a href="#contact" className="nav-button contacts-link p-1 rounded-lg hidden lg:block">CONTACT</a>
         <a href="logo.png" download className="nav-button contacts-link p-1 rounded-lg hidden lg:block">RESUME</a>
       </div>
-        <div className="hamburger-menu block lg:hidden">
-          <GiHamburgerMenu />
-        </div>
+      <input ref={hamburgerRef} onChange={changeVisibility} type="checkbox" className="hamburger-btn btn btn-ghost mr-4 hamburger-menu lg:hidden flex justify-end items-end" />
+      {hamburgerIsActive &&
+      <ul className="hamburger-menu text-lg absolute right-0 p-2 mt-64 shadow rounded-box w-48 flex flex-col text-center" style={{backgroundColor: "#222831"}}>
+        <li className="p-2 rounded-lg bg-slate-600 w-full mb-2"><a href="#home">About</a></li>
+        <li className="p-2 rounded-lg bg-slate-600 w-full mb-2"><a href="#projects">Projects</a></li>
+        <li className="p-2 rounded-lg bg-slate-600 w-full mb-2"><a href="#contact">Contact</a></li>
+        <li className="p-2 rounded-lg bg-slate-600 w-full"><a href="logo.png">Resume</a></li>
+      </ul>}
     </nav>
   )
 }
--- portfolio-website/src/components/pages/Contact.jsx
+++ portfolio-website/src/components/pages/Contact.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div id="contact" style={{height: "100vh", width: "100%"}} className="article">Contact</div>
+    <div id="contact" style={{height: "100vh", width: "100%"}} className="article flex flex-row">
+      <footer className="footer footer-center text-base-content rounded self-end p-6">
+          <p>Copyright © 2023 - All right reserved by Bartosz Tobiński</p>
+      </footer>
+    </div>
   )
 }
 
