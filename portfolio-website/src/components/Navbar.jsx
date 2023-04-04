be054b5fdbe741bd317d5c810ac86f0030a98a69
bttobi <bttobi123@gmail.com>
2023-04-04 19:33:59 +0200

    fixed hamburger menu styling

--- portfolio-website/src/components/Navbar.jsx
+++ portfolio-website/src/components/Navbar.jsx
@@ -9,9 +9,9 @@ const Navbar = () => {
   }
   return (
     <nav className="navbar backdrop-blur-sm bg-white/10 flex flex-row align-center justify-start fixed m-0 p-0 text-white w-full h-10 z-20 text-3xl">
-      <div className="home-wrapper h-full flex justify-center grow whitespace-nowrap">
-        <a href="#home" className="name-surname home-page font-bold ml-6 flex align-center justify-center items-center">
-          <img className="logo lg:ml-4" src="./logo.png" alt="bartosz tobinski logo picture" width="60px"/>
+      <div className="home-wrapper h-full w-min flex justify-center grow whitespace-nowrap">
+        <a href="#home" className="name-surname home-page w-16 lg:w-min font-bold lg:ml-6 flex align-center justify-center items-center">
+          <img className="logo ml-0 lg:ml-4" src="./logo.png" alt="bartosz tobinski logo picture" width="60px"/>
           <p className="nav-button name-and-surname ml-4 p-1 hidden lg:block rounded-lg">BARTOSZ TOBIŃSKI</p>
         </a>
       </div>
@@ -20,15 +20,17 @@ const Navbar = () => {
         <a href="#contact" className="nav-button contacts-link p-1 rounded-lg hidden lg:block">CONTACT</a>
         <a href="logo.png" download className="nav-button contacts-link p-1 rounded-lg hidden lg:block">RESUME</a>
       </div>
-      <input ref={hamburgerRef} onChange={changeVisibility} type="checkbox" className="hamburger-btn btn btn-ghost mr-4 hamburger-menu lg:hidden flex justify-end items-end" />
+      <label className="hamburger-btn mr-1 h-3/4 rounded-lg lg:hidden cursor-pointer">
+        <input className="cursor-pointer absolute" ref={hamburgerRef} onChange={changeVisibility} type="checkbox"/>
+      </label>
       <AnimatePresence>
         {hamburgerIsActive &&
-        <motion.div initial={{transform: "scale(0)", opacity: 0}} animate={{transform: "scale(1)", opacity: 1}} exit={{transform: "scale(0)", opacity: 0}}>
-          <ul className="hamburger-menu text-lg absolute right-0 p-2 mt-64 shadow rounded-box w-48 flex flex-col text-center justify-center items-center" style={{backgroundColor: "#222831"}}>
-            <li className="p-1 rounded-lg bg-slate-600 w-40 mb-2"><a href="#home">About</a></li>
-            <li className="p-1 rounded-lg bg-slate-600 w-40 mb-2"><a href="#projects">Projects</a></li>
-            <li className="p-1 rounded-lg bg-slate-600 w-40 mb-2"><a href="#contact">Contact</a></li>
-            <li className="p-1 rounded-lg bg-slate-600 w-40"><a href="logo.png">Resume</a></li>
+        <motion.div className="cursor-pointer" initial={{y: "-4rem", opacity: 0}} animate={{y: "0rem", opacity: 1}} exit={{y: "-4rem", opacity: 0}}>
+          <ul className="hamburger-menu text-lg lg:hidden absolute right-0 p-2 mt-64 shadow rounded-box w-48 flex flex-col text-center justify-center items-center" style={{backgroundColor: "#222831"}}>
+            <a href="#home"><li className="menu-item p-1 rounded-lg bg-slate-600 w-40 mb-2">About</li></a>
+            <a href="#projects"><li className="menu-item p-1 rounded-lg bg-slate-600 w-40 mb-2">Projects</li></a>
+            <a href="#contact"><li className="menu-item p-1 rounded-lg bg-slate-600 w-40 mb-2">Contact</li></a>
+            <a href="logo.png"><li className="menu-item p-1 rounded-lg bg-slate-600 w-40">Resume</li></a>
           </ul>
         </motion.div>}
       </AnimatePresence>
--- portfolio-website/src/components/data/projects.jsx
+++ portfolio-website/src/components/data/projects.jsx
@@ -7,18 +7,24 @@ export const data = [
     id: "project-1",
     title: "Coming Soon",
     scene: <QuestionBlockScene />,
-    desc: "Coming Soon"
+    desc: "Coming Soon",
+    demo_url: "#demo",
+    github_url: "#github"
   },
   {
     id: "project-2",
     title: "Cat Page",
     scene: <CatDispScene />,
-    desc: "Coming Soon"
+    desc: "Page made for cat lovers. You can create an account, search for cat breeds and funny gifs as well as add them to favourites!",
+    demo_url: "#demo",
+    github_url: "#github"
   },
   {
     id: "project-3",
     title: "Coming Soon",
     scene: <BananyaScene />,
-    desc: "Coming Soon"
+    desc: "Coming Soon",
+    demo_url: "#demo",
+    github_url: "#github"
   }
 ];
