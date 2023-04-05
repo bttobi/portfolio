1f28613b9263b2d5b05149799ccb54f13691b264
bttobi <bttobi123@gmail.com>
2023-04-05 23:35:11 +0200

    added tech array to specify used tech in a project

--- portfolio-website/src/components/data/projects.jsx
+++ portfolio-website/src/components/data/projects.jsx
@@ -5,26 +5,29 @@ import QuestionBlockScene from '../scenes/QuestionBlockScene';
 export const data = [
   {
     id: "project-1",
-    title: "Coming Soon",
+    name: "Coming Soon",
     scene: <QuestionBlockScene />,
     desc: "Coming Soon",
     demo_url: "#demo",
-    github_url: "#github"
+    github_url: "#github",
+    tech: []
   },
   {
     id: "project-2",
-    title: "Cat Page",
+    name: "Cat Page",
     scene: <CatDispScene />,
     desc: "Page made for cat lovers. You can create an account, search for cat breeds and funny gifs as well as add them to favourites!",
     demo_url: "#demo",
-    github_url: "#github"
+    github_url: "#github",
+    tech: ["React.js", "Firebase", "React Query", "Tailwind CSS", "daisyUI", "React Router"]
   },
   {
     id: "project-3",
-    title: "Coming Soon",
+    name: "Coming Soon",
     scene: <BananyaScene />,
     desc: "Coming Soon",
     demo_url: "#demo",
-    github_url: "#github"
+    github_url: "#github",
+    tech: []
   }
 ];
