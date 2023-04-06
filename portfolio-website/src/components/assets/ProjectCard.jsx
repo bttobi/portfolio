67217b042ae9aef2b10b7d4a54f526f482b9cbb7
bttobi <bttobi123@gmail.com>
2023-04-06 17:32:39 +0200

    styling tweaks

--- portfolio-website/src/components/assets/ProjectCard.jsx
+++ portfolio-website/src/components/assets/ProjectCard.jsx
@@ -5,11 +5,11 @@ import CatDispScene from '../scenes/CatDispScene';
 const ProjectCard = ({projectInfo}) => {
   return (
     <>
-      <label htmlFor={projectInfo.id} className="btn shadow-lg hover:scale-105 shadow-black border-none w-1/3 h-1/3 mt-10 lg:w-1/5 lg:h-3/4 flex flex-col p-0 mx-0 lg:mx-8 xl:mx-16 justify-start bg-gradient-to-r from-cyan-600 to-cyan-400 text-white">
+      <label htmlFor={projectInfo.id} className="btn shadow-lg hover:scale-105 shadow-black border-none w-1/3 h-1/3 mt-10 lg:w-1/5 lg:h-3/4 flex flex-col p-0 mx-0 lg:mx-8 xl:mx-16 justify-start bg-gradient-to-r from-cyan-600 to-cyan-400 text-white z-10">
         <div className="rounded-t-lg w-full h-full flex flex-col align-center justify-center items-center">
           <div className="absolute flex flex-col align-center justify-center items-center z-10 text-white lg:w-60 w-min">
-            <p className="flex p-2 justify-center flex-wrap lg:text-xl text-md mt-0 lg:mt-10 z-20 rounded-lg mb-4 lg:mb-0" style={{backgroundColor: "#1b262c8f"}}>{projectInfo.name}</p>
-            <p className="flex justify-center flex-wrap  mt-10 z-20 p-1 rounded-lg" style={{backgroundColor: "#1b262c8f"}}>Click to see the details</p>
+            <p className="flex p-2 justify-center flex-wrap lg:text-xl text-md mt-0 lg:mt-10 z-20 rounded-lg mb-4 lg:mb-0 bg-blue-darker bg-opacity-60">{projectInfo.name}</p>
+            <p className="flex justify-center flex-wrap  mt-10 z-20 p-1 rounded-lg bg-blue-darker bg-opacity-60">Click to see the details</p>
           </div>
           {projectInfo.scene}
         </div>
--- portfolio-website/src/components/assets/ProjectCardClicked.jsx
+++ portfolio-website/src/components/assets/ProjectCardClicked.jsx
@@ -7,7 +7,7 @@ const ProjectCardClicked = ({projectInfo}) => {
     <>
       <input type="checkbox" className="modal-toggle" id={projectInfo.id} />
       <label htmlFor={projectInfo.id} className="modal cursor-pointer">
-        <label htmlFor="" className="modal-box h-min bg-gradient-to-r from-cyan-600 to-cyan-400">
+        <label className="modal-box h-min bg-gradient-to-r from-cyan-600 to-cyan-400">
           <label htmlFor={projectInfo.id} className="close-btn cursor-pointer rounded-lg p-2 absolute right-3 top-3 bg-red-500 "><AiOutlineCloseCircle /></label>
           <p className="font-bold text-xl text-center">{projectInfo.name}</p>
           <p className="py-4 text-center">{projectInfo.desc}</p>
@@ -18,10 +18,10 @@ const ProjectCardClicked = ({projectInfo}) => {
           </ul>
           {projectInfo.name != "Coming Soon" &&
           <div className="button-wrapper flex align-center items-center justify-around">
-            <a href={projectInfo.demo_url} className="modal-action btn border-none font-bold p-3 rounded-lg w-min text-white">
+            <a href={projectInfo.demo_url} className="modal-action btn border-none font-bold p-3 rounded-lg w-min text-white" target="_blank">
               Demo
             </a>
-            <a href={projectInfo.github_url} className="modal-action btn border-none font-bold p-3 rounded-lg w-min text-white">
+            <a href={projectInfo.github_url} className="modal-action btn border-none font-bold p-3 rounded-lg w-min text-white" target="_blank">
               Github
             </a>
           </div>} 
