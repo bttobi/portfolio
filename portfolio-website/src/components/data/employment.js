1820c987113791487e0939efb86936d91ea45761
bttobi <bttobi123@gmail.com>
2023-04-06 17:33:19 +0200

    created employment.js for easier work history storing

---
+++ portfolio-website/src/components/data/employment.js
@@ -0,0 +1,10 @@
+export const data = [
+  {
+    company: "GSK",
+    position: "It Automation Trainee",
+    date: "10.2022 - currently",
+    logo_url: "https://upload.wikimedia.org/wikipedia/en/thumb/3/32/GSK_logo_2022.svg/250px-GSK_logo_2022.svg.png?20220614091439",
+    img_width: "128",
+    img_height: "48"
+  }
+]
