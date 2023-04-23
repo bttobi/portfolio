1b97c5935096bef686df7d7ddd8a26a279ac80a5
bttobi <bttobi123@gmail.com>
2023-04-23 14:58:28 +0200

    mobile responsiveness sizing changes

--- src/components/data/projects.jsx
+++ src/components/data/projects.jsx
@@ -1,19 +1,9 @@
 import CatDispScene from '../scenes/CatDispScene';
-import BananyaScene from '../scenes/BananyaScene';
 import QuestionBlockScene from '../scenes/QuestionBlockScene';
 
 export const data = [
   {
     id: "project-1",
-    name: "Coming Soon",
-    scene: <QuestionBlockScene />,
-    desc: "Coming Soon",
-    demo_url: "#demo",
-    github_url: "#github",
-    tech: []
-  },
-  {
-    id: "project-2",
     name: "Cat Page",
     scene: <CatDispScene />,
     desc: "Page made for cat lovers. You can create an account, search for cat breeds and funny gifs as well as add them to favourites!",
@@ -22,9 +12,18 @@ export const data = [
     tech: ["React.js", "Firebase", "React Query", "Tailwind CSS", "daisyUI", "React Router"]
   },
   {
+    id: "project-2",
+    name: "More Coming Soon",
+    scene: <QuestionBlockScene />,
+    desc: "Coming Soon",
+    demo_url: "#demo",
+    github_url: "#github",
+    tech: []
+  },
+  {
     id: "project-3",
-    name: "Coming Soon",
-    scene: <BananyaScene />,
+    name: "More Coming Soon",
+    scene: <QuestionBlockScene />,
     desc: "Coming Soon",
     demo_url: "#demo",
     github_url: "#github",
