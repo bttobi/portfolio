cab63e943b141e0a3880d112d984de40592736ed
bttobi <bttobi123@gmail.com>
2023-04-23 14:58:56 +0200

    added background for technologies, new font, button styling

--- src/components/assets/ProjectCardClicked.jsx
+++ src/components/assets/ProjectCardClicked.jsx
@@ -7,27 +7,23 @@ const ProjectCardClicked = ({projectInfo}) => {
     <>
       <input type="checkbox" className="modal-toggle" id={projectInfo.id} />
       <label htmlFor={projectInfo.id} className="modal cursor-pointer">
-        <label className="modal-box h-min bg-gradient-to-r from-cyan-600 to-cyan-400">
+        <label className="modal-box h-min bg-gradient-to-r from-cyan-600 to-cyan-400 px-0">
           <button aria-label="Close modal">
             <label htmlFor={projectInfo.id} className="close-btn cursor-pointer rounded-lg p-2 absolute right-3 top-3 bg-red-500 "><GrClose fill={"white"} size={20}/></label>
           </button>
           <p className="font-bold text-xl text-center">{projectInfo.name}</p>
-          <p className="py-4 text-center">{projectInfo.desc}</p>
-          <ul className="mt-4 flex flex-row flex-wrap align-center items-baseline justify-around">
+          <p className="p-4 text-center">{projectInfo.desc}</p>
+          {projectInfo.tech.length != 0 &&
+          <ul className="mt-4 flex flex-row flex-wrap align-center items-baseline justify-around text-black bg-white py-4 shadow-blue-dark">
             {projectInfo.tech.map((techName, index) => 
               <TechIcon name={techName} key={index}/>
             )}
-          </ul>
-          {projectInfo.name != "Coming Soon" && //to be implemented
-          <div className="button-wrapper flex align-center items-center justify-around">
-            <button className="mt-8" aria-label="Open project demo">
-              <a href={projectInfo.demo_url} className="modal-action btn border-none font-bold p-3 m-0 rounded-lg w-min text-white" target="_blank">
-                Demo
-              </a>
-            </button>
-            <button className="mt-8" aria-label="Open Github repository">
-              <a href={projectInfo.github_url} className="modal-action btn border-none font-bold p-3 m-0 rounded-lg w-min text-white" target="_blank">
-                Github
+          </ul>}
+          {projectInfo.name != "More Coming Soon" && //to be implemented
+          <div className="button-wrapper flex flex-col align-center items-center justify-around font-main mx-8">
+            <button className="mt-4 mb-0 flex justify-center items-center align-center content-center" aria-label="Open project demo">
+              <a href={projectInfo.demo_url} className="modal-action btn border-none font-bold p-4 m-0 rounded-lg h-full text-white text-center flex justify-center lg:text-lg text-sm" target="_blank">
+                Project Demo
               </a>
             </button>
           </div>} 
