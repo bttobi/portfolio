675e859f5d703e142fd2d98c9a6525d790c2d0aa
bttobi <bttobi123@gmail.com>
2023-04-23 14:59:20 +0200

    styling changes for better visibility

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -26,7 +26,7 @@ const Navbar = () => {
     <nav className="navbar backdrop-blur-sm bg-white/10 flex flex-row align-center justify-start fixed m-0 p-0 text-white w-full h-10 z-20 text-3xl">
       <div className="home-wrapper h-full w-min flex justify-between grow whitespace-nowrap">
         <button aria-label="Go to about section">
-          <a href="#home" className="name-surname home-page w-16 lg:w-min font-bold lg:ml-6 flex align-center justify-center items-center">
+          <a href="#home" className="name-surname home-page w-16 lg:w-min lg:ml-6 flex align-center justify-center items-center">
             <img className="logo ml-0 lg:ml-4" src="https://i.imgur.com/otXNjSw.png" alt="bartosz tobinski logo picture" width="60px"/>
             <p className="nav-button active-nav name-and-surname ml-4 p-1 hidden lg:block rounded-lg">BARTOSZ TOBIŃSKI</p>
           </a>
@@ -47,7 +47,7 @@ const Navbar = () => {
       <AnimatePresence>
         {hamburgerIsActive &&
         <motion.div className="cursor-pointer" initial={{y: "-4rem", opacity: 0}} animate={{y: "0rem", opacity: 1}} exit={{y: "-4rem", opacity: 0}}>
-          <ul className="hamburger-menu text-lg lg:hidden absolute right-0 p-4 mt-64 shadow rounded-box w-48 flex flex-col text-center justify-center items-center bg-blue-darker">
+          <ul className="hamburger-menu text-lg lg:hidden absolute right-0 p-4 mt-64 shadow rounded-box w-48 flex flex-col text-center justify-center items-center bg-blue-darker border-white border-2">
             <li className="menu-item flex rounded-lg bg-slate-600 w-full h-full mb-2">
                 <a aria-label="Go to about section" href="#home" className="text-center w-full h-full p-1">About</a>
             </li>
