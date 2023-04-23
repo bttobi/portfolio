b3340f57bc167e054b662a0dec49a8b7bcccdfad
bttobi <bttobi123@gmail.com>
2023-04-23 15:01:20 +0200

    styling and animation tweaks

--- src/components/scenes/models_jsx/HelloText.jsx
+++ src/components/scenes/models_jsx/HelloText.jsx
@@ -1,17 +1,11 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import { useGLTF } from '@react-three/drei';
-import { useFrame } from '@react-three/fiber';
 
 export default function HelloText(props) {
   const { nodes, materials } = useGLTF('/models/HelloModel/HelloText-transformed.glb');
-  const textRef = useRef();
-
-  useFrame(()=>{
-    textRef?.current?.rotateY(0.001);
-  });
 
   return (
-    <group position={[0, -1, 0]} ref={textRef} {...props} dispose={null} scale={0.06}>
+    <group position={[0, -1, 0]} {...props} dispose={null} scale={0.06}>
       <group rotation={[-Math.PI / 2, 0, 0]}>
         <mesh geometry={nodes['3D_Text_-_H'].geometry} material={materials['Romeo 1']} position={[-58.88, 7.5, 0.8]} />
         <mesh geometry={nodes['3D_Text_-_e'].geometry} material={materials['Romeo 1']} position={[-27.96, 7.5, 0.8]} />
