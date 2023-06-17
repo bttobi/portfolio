45b15ba821f06d79fa638e6649539d217457ceae
bttobi <bttobi123@gmail.com>
2023-06-17 23:56:33 +0200

    updated styling of torusknot scene

--- src/components/scenes/TorusKnotScene.jsx
+++ src/components/scenes/TorusKnotScene.jsx
@@ -1,23 +1,19 @@
 import React, { Suspense } from "react";
-import { OrbitControls, Preload, BakeShadows } from "@react-three/drei";
+import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import TorusKnot from "./models_jsx/TorusKnot";
 import Loading from "../assets/Loading";
 
 const TorusKnotScene = () => {
   return (
-    <div className="lg:order-2 order-1 w-full lg:h-96 md:h-72 h-48 lg:m-0 cursor-pointer">
-      <Suspense fallback={<Loading />}>
-        <Canvas>
-          <ambientLight intensity={0.5} />
-          <pointLight intensity={2} position={[10, 0, 10]} />
-          <OrbitControls enableZoom={false} />
-          <TorusKnot />
-          <Preload all />
-          <BakeShadows />
-        </Canvas>
-      </Suspense>
-    </div>
+    <Suspense fallback={<Loading />}>
+      <Canvas>
+        <OrbitControls enableZoom={false} />
+        <ambientLight intensity={0.5} />
+        <pointLight intensity={2} position={[10, 0, 10]} />
+        <TorusKnot />
+      </Canvas>
+    </Suspense>
   );
 };
 
