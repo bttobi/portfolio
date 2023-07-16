997df2c6c1ba52ab2fff5f8868b4a754c24bb717
bttobi <bttobi123@gmail.com>
2023-07-16 16:48:09 +0200

    updated my current workplace!

--- src/components/data/employment.js
+++ src/components/data/employment.js
@@ -2,9 +2,17 @@ export const data = [
   {
     company: "GSK",
     position: "IT AUTOMATION",
-    date: "10.2022 - currently",
+    date: "10.2022 - 07.2023",
     logo_url:
       "https://upload.wikimedia.org/wikipedia/en/thumb/3/32/GSK_logo_2022.svg/250px-GSK_logo_2022.svg.png?20220614091439",
+    img_width: "128",
+    img_height: "48",
+  },
+  {
+    company: "SymphonyAI",
+    position: "SOFTWARE ENGINEER",
+    date: "07.2023 - currently",
+    logo_url: "https://i.imgur.com/N8wG6p3.png",
     img_width: "128",
     img_height: "48",
   },
