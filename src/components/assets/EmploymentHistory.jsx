0d17d0c7a3925169ca26b1c2d700fc70454c5f57
bttobi <bttobi123@gmail.com>
2023-07-16 16:48:22 +0200

    updated styling, added an arrow

--- src/components/assets/EmploymentHistory.jsx
+++ src/components/assets/EmploymentHistory.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { GrClose } from "react-icons/gr";
+import { BsFillArrowDownCircleFill } from "react-icons/bs";
 import { data } from "../data/employment";
 
 const EmploymentHistory = () => {
@@ -18,34 +19,39 @@ const EmploymentHistory = () => {
               <GrClose size={20} />
             </label>
           </button>
+          <p className="text-xl text-center text-white font-article mt-2">
+            For more information click
+          </p>
           <ul className="flex flex-col justify-center align-center items-center mt-2 mb-2 w-auto">
-            <p className="text-xl text-center text-white font-article">
-              For more information click
-            </p>
             {workHistory.map((el, index) => (
-              <li
-                key={index}
-                className="employment-history-item rounded-lg pt-2 font-bold px-4 w-auto h-min bg-white border-0 mt-4 transition-all duration-150 hover:bg-slate-200 active:scale-95 active:bg-blue-light font-article shadow-md shadow-blue-dark"
-              >
-                <button className="font-primary" aria-label="Go to LinkedIn">
-                  <a
-                    className="flex flex-col justify-center items-center font-primary"
-                    href="https://www.linkedin.com/in/bartosz-tobinski/"
-                    target="_blank"
-                  >
-                    <img
-                      className="company-logo mb-2"
-                      src={el.logo_url}
-                      width={el.img_width}
-                      alt={el.company + " logo"}
-                    />
-                    <p className="text-xl text-center text-blue-dark">
-                      {el.position}
-                    </p>
-                    <p className="pb-2 text-center text-black">{el.date}</p>
-                  </a>
-                </button>
-              </li>
+              <>
+                <li
+                  key={index}
+                  className="employment-history-item rounded-lg pt-2 font-bold px-4 w-64 flex flex-col justify-center h-min bg-white border-0 mt-4 transition-all duration-150 hover:bg-slate-200 active:scale-95 active:bg-blue-light font-article shadow-md shadow-blue-dark"
+                >
+                  <button className="font-primary" aria-label="Go to LinkedIn">
+                    <a
+                      className="flex flex-col justify-center items-center font-primary"
+                      href="https://www.linkedin.com/in/bartosz-tobinski/"
+                      target="_blank"
+                    >
+                      <img
+                        className="company-logo mb-2"
+                        src={el.logo_url}
+                        width={el.img_width}
+                        alt={el.company + " logo"}
+                      />
+                      <p className="text-xl text-center text-blue-dark">
+                        {el.position}
+                      </p>
+                      <p className="pb-2 text-center text-black">{el.date}</p>
+                    </a>
+                  </button>
+                </li>
+                {index + 1 != workHistory.length && (
+                  <BsFillArrowDownCircleFill size={32} className="mt-4" />
+                )}
+              </>
             ))}
           </ul>
         </label>
